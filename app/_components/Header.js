21b1a672377145ff3e7f9a931cf7aceb5f8201a4
MAKANAKI <chidoziemail@gmail.com>
2024-05-22 14:54:27 +0100

    dark theme

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -7,7 +7,7 @@ import DarkModeToggle from './DarkModeToggle'
 function Header() {
   return (
     <div><header className="fixed top-0 w-full bg-white dark:bg-gray-900">
-  <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8 border-b border-b-gray-700">
+  <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8 border-b borderbg">
     <a className="block text-teal-600" href="#">
       <span className="sr-only">Home</span>
       <svg className="h-8" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -22,23 +22,23 @@ function Header() {
       <nav aria-label="Global" className="hidden md:block">
         <ul className="flex items-center gap-6 text-sm">
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Home </a>
+            <a className="text-gray-500 transition hover:text-primary dark:hover:text-gray-300/75" href="#"> Home </a>
           </li>
 
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#">Upload</a>
+            <a className="text-gray-500 transition hover:text-primary dark:hover:text-gray-300/75" href="#">Upload</a>
           </li>
 
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> History </a>
+            <a className="text-gray-500 transition hover:text-primary dark:hover:text-gray-300/75" href="#"> History </a>
           </li>
 
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Contact us </a>
+            <a className="text-gray-500 transition hover:text-primary dark:hover:text-gray-300/75" href="#"> Contact us </a>
           </li>
 
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> About us </a>
+            <a className="text-gray-500 transition hover:text-primary dark:hover:text-gray-300/75" href="#"> About us </a>
           </li>
 
         </ul>
--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-    <div className=" bg-slate-200 dark:bg-gray-950">
+    <div>
       <Header />
       <Hero />
     </div>
